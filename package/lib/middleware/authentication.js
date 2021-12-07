7bf24cd3a436cca9d6299cc1f4b715cab65f486e
Paul Robert Lloyd <me+git@paulrobertlloyd.com>
2021-12-07 21:31:14 +0000

    Check auth config in server.js like other checks

--- package/lib/middleware/authentication.js
+++ package/lib/middleware/authentication.js
@@ -9,22 +9,19 @@ import basicAuth from 'basic-auth'
  * @returns {Function} Express middleware requiring the given credentials
  */
 export function authentication (req, res, next) {
-  const { env, useAuth } = res.app.locals
   const username = process.env.USERNAME
   const password = process.env.PASSWORD
 
-  if (env === 'production' && useAuth) {
-    if (!username || !password) {
-      console.error('Username or password is not set')
-      return res.send('Username or password is not set')
-    }
+  if (!username || !password) {
+    console.error('Username or password is not set')
+    return res.send('Username or password is not set')
+  }
 
-    const user = basicAuth(req)
+  const user = basicAuth(req)
 
-    if (!user || user.name !== username || user.pass !== password) {
-      res.set('WWW-Authenticate', 'Basic realm=Authorization Required')
-      return res.sendStatus(401)
-    }
+  if (!user || user.name !== username || user.pass !== password) {
+    res.set('WWW-Authenticate', 'Basic realm=Authorization Required')
+    return res.sendStatus(401)
   }
 
   next()
