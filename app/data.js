9f7cfa434fefcc356f6ebf3d781934f7b0121e0e
Jake Benilov <benilov@gmail.com>
2021-12-09 21:41:14 +0000

    Example prepopulated data (for taking design history screenshots)

--- app/data.js
+++ app/data.js
@@ -24,4 +24,19 @@ export default {
     'itt-provider': 'Gorse SCITT',
     'trn': '0000000'
   }
+  // 'email-address': 'jsmith@example.com',
+  // 'email-address': 'jsmith@myschool.gov.uk',
+  // 'first-name': 'Jane',
+  // 'last-name': 'Smith',
+  // 'changed-name': 'changed-name',
+  // 'previous-first-name': 'Jane',
+  // 'previous-last-name': 'Doe',
+  // 'date-of-birth': { day: 27, month: 3, year: 1987 },
+  // 'national-insurance-number': 'QQ 12 34 56 C',
+  // 'itt-provider': 'Gorse SCITT',
+  // 'trn': '0000000',
+  // 'has-national-insurance-number': 'yes',
+  // 'wizard': {
+  //   'has-done-itt': 'yes'
+  // }
 }
