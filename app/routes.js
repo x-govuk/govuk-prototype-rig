2403a3abbab0cc69b7d17a747070496bd37ff674
Paul Hayes <fofr@users.noreply.github.com>
2022-02-25 13:45:03 +0000

    Simplify wizard implementation
    We don't need to define the views we render, as the existing prototype rig code knows which view based on the path

--- app/routes.js
+++ app/routes.js
@@ -11,23 +11,6 @@ router.all('/examples/wizard/:view?', (req, res, next) => {
   next()
 })
 
-router.get('/examples/wizard/:view?', (req, res, next) => {
-  const view = req.params.view || 'index'
-  const views = [
-    'check-answers',
-    'confirm',
-    'england',
-    'index',
-    'name',
-    'nationality',
-    'where-do-you-live'
-  ]
-
-  if (views.includes(view)) {
-    res.render(`examples/wizard/${view}`)
-  }
-})
-
 router.post('/examples/wizard/:view?', (req, res) => {
   res.redirect(res.locals.paths.next)
 })
--- app/wizards.js
+++ app/wizards.js
@@ -1,7 +1,6 @@
 import { wizard } from 'govuk-prototype-rig'
 
 export function exampleWizard (req) {
-  const { data } = req.session
   const journey = {
     '/examples/wizard': {},
     '/examples/wizard/name': {},
@@ -9,7 +8,10 @@ export function exampleWizard (req) {
       // Example fork in the journey
       // Go to nationality page if answer to the question ‘Where do you live?’
       // is not England
-      '/examples/wizard/nationality': () => data.wizard['where-do-you-live'] !== 'England'
+      '/examples/wizard/nationality': {
+        data: 'wizard.where-do-you-live',
+        excludedValue: 'England'
+      }
     },
     '/examples/wizard/england': {},
     '/examples/wizard/nationality': {},
